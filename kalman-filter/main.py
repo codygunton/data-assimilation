d923e88ee0d8d8ad43c64d3bf86465ab00b18bca
Cody Gunton <codygunton@gmail.com>
2017-09-27 16:08:31 -0700

    Tweaked graphs and improved computation of Kalman gain.

--- kalman-filter/main.py
+++ kalman-filter/main.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 # we work with the dampened oscillator d^2z/dt^2 + 2w(xi)dz/dt + w^2z = 0
 # we converted to a system in x = (z, dz/dt)
 
+
 # global parameters
-# good values to see dampening: w = 2., xi = 0.1
 T = 25000                   # number of time steps
 dt = 0.001                  # time step
 w = 1.                      # omega
 xi = 0.001                  # xi
 H = np.array([[1., 0]])     # measurement matrix
-R = 5                       # variance of measurement error
+R = np.array(5)             # variance of measurement error
 I = np.identity(2)          # identity matrix
 x0 = np.random.multivariate_normal([0, 0], I)  # initial condition
 
@@ -21,7 +21,7 @@ M = I + dt * A                   # forecasting matrix
 
 # generate numerical solution and plot
 # to illustrate dampening, stack n plots of width T
-n = 2
+n = 30
 times = [0 + k * dt for k in range(n*T+1)]
 true_xs = [x0]
 for k in range(n*T):
@@ -29,10 +29,23 @@ for k in range(n*T):
 true_zs = [x[0] for x in true_xs]
 true_zts = [x[1] for x in true_xs]
 
-plt.figure(figsize=(60, 5))
-plt.scatter(times, true_zs, s=1, c=(0, 0, 1))
-plt.scatter(times, true_zts, s=1, c=(1, 0, 0))
-plt.legend(['z', 'dz/dt'], markerscale=4)
+fig, z_axis = plt.subplots(figsize=(60, 6))
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+fig.suptitle('Components of the system derived from '
+             r"$\displaystyle \frac{d^2z}{dt^2} + 2\omega\xi \frac{dz}{dt} + \omega^2 z = 0$", fontsize=18, fontweight='bold')
+z_axis.scatter(times, true_zs, s=1, c=(0, 0, 1))
+z_axis.set_xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=18, fontweight='bold')
+# Make the y-axis label, ticks and tick labels match the line color.
+z_axis.set_ylabel(r"$z$" ' (position)', color='b',
+                  fontsize=18, fontweight='bold')
+z_axis.tick_params('y', colors='b')
+zt_axis = z_axis.twinx()
+zt_axis.scatter(times, true_zts, s=1, c=(1, 0, 0))
+zt_axis.set_ylabel(r"$z_t$" ' (velocity)', color='r', fontsize=18,
+                   fontweight='bold')
+zt_axis.tick_params('y', colors='r')
 plt.show()
 
 
@@ -45,16 +58,23 @@ true_zts = true_zts[:T+1]
 
 # generate synthetic data and plot
 synth_ys = [H @ x + np.random.normal(0, np.sqrt(R)) for x in true_xs]
-
 plt.figure(figsize=(8, 8))
 plt.scatter(times, synth_ys, s=1, c=(0, 1, 0))
 plt.scatter(times, true_zs, s=1, c=(0, 0, 1))
+plt.suptitle('True position data and synthetic position data',
+             fontsize=14, fontweight='bold')
+
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
+plt.ylabel('position', fontsize=14, fontweight='bold')
 plt.legend(['Synthetic z', 'True z'], markerscale=4)
 plt.show()
 
 
 # kalman filter
-kalman_mus = [[0, 0]]
+# initialize lists of analysis means (2x1 column vectors)
+# covariacnes (2x2 matrices), and gains (2x1 column vectors)
+kalman_mus = [np.array([[0, 0]]).T]
 kalman_Ps = [I]
 kalman_gains = []
 for k in range(T):
@@ -65,7 +85,8 @@ for k in range(T):
     # forecasting step
     mu_f = M @ mu_last
     P_f = M @ P_last @ M.T
-    K = P_f @ H.T @ np.linalg.inv(R + H @ P_f @ H.T)
+    # here R.T + (H @ P_f @ H.T).T is 1x1, but this generalizes
+    K = np.linalg.solve(R.T + (H @ P_f @ H.T).T, H @ P_f.T).T
     kalman_gains += [K]
 
     # form new analysis mean and covariance and append to lists
@@ -79,17 +100,26 @@ kalman_zts = [mu[1] for mu in kalman_mus]
 
 
 # plot kalman reconstruction of z
-plt.figure(figsize=(24, 4))
+plt.figure(figsize=(24, 6))
 plt.scatter(times, true_zs, s=1, c=(0, 0, 1))
 plt.scatter(times, kalman_zs, s=1, c=(0, 1, 0))
+plt.suptitle('True position data and Kalman reconstruction',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
+plt.ylabel('position', fontsize=14, fontweight='bold')
 plt.legend(['True z', 'Kalman reconstruction'], markerscale=4)
 plt.show()
 
-
 # plot kalman reconstruction of z_t
-plt.figure(figsize=(24, 4))
+plt.figure(figsize=(24, 6))
 plt.scatter(times, true_zts, s=1, c=(1, 0, 0))
 plt.scatter(times, kalman_zts, s=1, c=(0, 1, 0))
+plt.suptitle('True velocity data and Kalman reconstruction',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
+plt.ylabel('velocity', fontsize=14, fontweight='bold')
 plt.legend(['True dz/dt', 'Kalman reconstruction'], markerscale=4)
 plt.show()
 
@@ -100,12 +130,18 @@ K1s = [K[1] for K in kalman_gains]
 
 plt.figure(figsize=(12, 6))
 plt.scatter(times[1:], K0s, s=1, c='#ff6699')
-plt.legend(['First component of Kalman gain'], markerscale=4)
+plt.suptitle('First component of Kalman gain',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
 plt.show()
 
 plt.figure(figsize=(12, 6))
 plt.scatter(times[1:], K1s, s=1, c='#ff6699')
-plt.legend(['Second component of Kalman gain'], markerscale=4)
+plt.suptitle('Second component of Kalman gain',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
 plt.show()
 
 
@@ -118,5 +154,9 @@ plt.figure(figsize=(12, 6))
 plt.scatter(times, RMSEs, s=1, c='#ff6699')
 cov_traces = [np.sqrt(0.5 * np.trace(P)) for P in kalman_Ps]
 plt.scatter(times, cov_traces, s=1, c='#42f4ce')
+plt.suptitle('Root MSE of reconstruction and normalized trace of analysis covariances',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (thousands of time steps of size dt)',
+                  fontsize=14, fontweight='bold')
 plt.legend(['RMSE', 'Normalized trace of analysis covariance'], markerscale=4)
 plt.show()
