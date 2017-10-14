3dd9a03f4a0bcced044411b626a189f5b41deff1
Cody Gunton <codygunton@gmail.com>
2017-10-14 15:39:02 -0700

    State generation and working and scaling well.

--- ensemble-kalman-filter/data-generation.py
+++ ensemble-kalman-filter/data-generation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import cProfile
 
 # global parameters
 N = 40             # number of state variables
@@ -11,12 +12,29 @@ def f(A, t):  # A a numpy array of shape N
     return (np.roll(A, 1) - np.roll(A, -2)) * np.roll(A, -1) - A + F
 
 
+def g(A):  # A a numpy array of shape N
+    return (np.roll(A, 1) - np.roll(A, -2)) * np.roll(A, -1) - A + F
+
+
+def g2(A):
+    out = []
+    for i in range(len(A)):
+        out += [(A[(i+1)%N] - A[(i-2)%N]) * A[(i-1)%N] - A[i] + F]
+    return np.array(out)
+
 def RK_function(x):
-    k1 = f(x, 0)
-    k2 = f(x + h*k1/2, 0 + h/2)
-    k3 = f(x + h*k2/2, 0 + h/2)
-    k4 = f(x + h*k3, 0 + h)
+    k1 = g2(x)
+    k2 = g2(x + h*k1/2)
+    k3 = g2(x + h*k2/2)
+    k4 = g2(x + h*k3)
     return x + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+
+standard_basis = []
+for i in range(N):
+    e = np.zeros(N)
+    np.put(e, i, 1)
+    standard_basis += [e]
 
 
 def RK_matrix(N):
@@ -25,13 +43,13 @@ def RK_matrix(N):
         e = np.zeros(N)
         np.put(e, i, 1)
         standard_basis += [e]
-    k1 = f(standard_basis, 0)
-    k2 = f(standard_basis + h*k1/2, 0 + h/2)
-    k3 = f(standard_basis + h*k2/2, 0 + h/2)
-    k4 = f(standard_basis + h*k3, 0 + h)
-    result = standard_basis + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
-    return result.T
+    M_cols = []
+    for e in standard_basis:
+        M_cols += [RK_function(e)]
+    return np.matrix(M_cols).T
 
+
+M = RK_matrix(N)
 
 
 # for times in RK_times (1-D array), solve system with initial state x0
@@ -60,16 +78,17 @@ x0 = np.load("starting-data.npy")
 RK_times = np.arange(h, 10.0, h)  # number of time steps
 states = L96(x0, RK_times)
 
-# # plot true states
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.plot(states[:, 0], states[:, 1], states[:, 2])
-# ax.set_xlabel('$x_1$')
-# ax.set_ylabel('$x_2$')
-# ax.set_zlabel('$x_3$')
-# plt.show()
+# plot true states
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+states = np.array(states)
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot(states[:, 0], states[:, 1], states[:, 2])
+ax.set_xlabel('$x_1$')
+ax.set_ylabel('$x_2$')
+ax.set_zlabel('$x_3$')
+plt.show()
 
 # generate synthetic data
 # only observe every 0.1 seconds, so keep only every nth state, n=0.1/h
@@ -85,11 +104,16 @@ np.save("synthetic-obs", obs)
 
 
 # generate initial ensemble
-import cProfile
 h = 0.005                         # time step
 x0 = np.load("starting-data.npy")
-RK_times = np.arange(h, 200.0, h)  # number of time steps
-t0 = time.time()
+RK_times = np.arange(h, 1000.0, h)  # number of time steps
 cProfile.run('states = L96(x0, RK_times)')
-t1 = time.time()
+print("time:", t1-t0)
+
+
+# generate initial ensemble
+h = 0.005                         # time step
+x0 = np.load("starting-data.npy")
+RK_times = np.arange(h, 100.0, h)  # number of time steps
+cProfile.run('states = L96(x0, RK_times)')
 print("time:", t1-t0)
