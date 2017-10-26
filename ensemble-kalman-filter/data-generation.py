f27c880ee2e5546f7ec1f5042ad4095aa37a2d15
Cody Gunton <codygunton@gmail.com>
2017-10-26 14:46:20 -0700

    Made generate_states function.

--- ensemble-kalman-filter/data-generation.py
+++ ensemble-kalman-filter/data-generation.py
@@ -46,62 +46,67 @@ tf = time.time()
 print("Initial position . Time: {0}".format(tf-t0))
 
 
-# get true states to generate synthetic data
-T = 10
-t0 = time.time()
-x0 = np.load("initial-position.npy")
-# np.random.seed(2017)
-# x0 = np.array([np.random.normal() for i in range(N)])
-states = L96(x0, T, h)
-states = np.array(states)
-np.save("./true-states/true-states-T{0}".format(T), states)
-tf = time.time()
-print("True states generated. Time: {0}".format(tf-t0))
+def generate_states(T):
+    # get true states to generate synthetic data
+    t0 = time.time()
+    x0 = np.load("initial-position.npy")
+    # np.random.seed(2017)
+    # x0 = np.array([np.random.normal() for i in range(N)])
+    states = L96(x0, T, h)
+    states = np.array(states)
+    np.save("./true-states/true-states-T{0}".format(T), states)
+    tf = time.time()
+    print("True states generated. Time: {0}".format(tf-t0))
 
-# # plot true first three true states
-# import matplotlib.pyplot as plt
-# from mpl_toolkits.mplot3d import Axes3D
-# fig = plt.figure()
-# ax = fig.gca(projection='3d')
-# ax.plot(states[:, 0], states[:, 1], states[:, 2])
-# ax.set_xlabel('$x_1$')
-# ax.set_ylabel('$x_2$')
-# ax.set_zlabel('$x_3$')
-# plt.show()
+    # # plot true first three true states
+    # import matplotlib.pyplot as pltimport numpy as np
+    # from mpl_toolkits.mplot3d import Axes3D
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+    # ax.plot(states[:, 0], states[:, 1], states[:, 2])
+    # ax.set_xlabel('$x_1$')
+    # ax.set_ylabel('$x_2$')
+    # ax.set_zlabel('$x_3$')
+    # plt.show()
 
-# plot true state of i-th variable
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-i = 1
-states = np.load("./true-states/true-states-T{0}.npy".format(T))
-fig = plt.figure()
-# ax = fig.gca(projection='3d')
-times = np.arange(0, T, h)
-plt.plot(times, states[:, i-1])
-# plt.set_xlabel('$x_{0}$'.format(i))
-plt.show()
+    # # plot true state of i-th variable
+    # import matplotlib.pyplot as plt
+    # from mpl_toolkits.mplot3d import Axes3D
+    # i = 1
+    # states = np.load("./true-states/true-states-T{0}.npy".format(T))
+    # fig = plt.figure()
+    # # ax = fig.gca(projection='3d')
+    # times = np.arange(0, T, h)
+    # plt.plot(times, states[:, i-1])
+    # # plt.set_xlabel('$x_{0}$'.format(i))
+    # plt.show()
 
 
-# generate synthetic data
-# only observe every obs_gap seconds,
-# so keep only every nth state, n=0.1/h
-t0 = time.time()
-obs_gap = 0.05
-n = int(obs_gap // h)
-states = np.load("./true-states/true-states-T{0}.npy".format(T))
-obs = np.array(states[::n])
-obs = list(map(lambda A: np.delete(A, range(0, N, 2)), obs))
-obs = np.array(obs)
-# this is inefficient; could flatten against 0s
-# but I want to write in terms of R
-np.random.seed(2016)
-R = np.identity(N//2)
-epsilon = np.array([np.random.multivariate_normal(np.zeros(N//2), R)
-                    for y in obs])
-obs = obs + epsilon
-np.save("./synthetic-observations/synthetic-obs-T{0}".format(T), obs)
-tf = time.time()
-print("Synthetic data generated. Time: {0}".format(tf-t0))
+def generate_data(T)
+    # generate synthetic data
+    # only observe every obs_gap seconds,
+    # so keep only every nth state, n=0.1/h
+    t0 = time.time()
+    obs_gap = 0.05
+    n = int(obs_gap // h)
+    states = np.load("./true-states/true-states-T{0}.npy".format(T))
+    obs = np.array(states[::n])
+    obs = list(map(lambda A: np.delete(A, range(0, N, 2)), obs))
+    obs = np.array(obs)
+    # this is inefficient; could flatten against 0s
+    # but I want to write in terms of R
+    np.random.seed(2016)
+    R = np.identity(N//2)
+    epsilon = np.array([np.random.multivariate_normal(np.zeros(N//2), R)
+                        for y in obs])
+    obs = obs + epsilon
+    np.save("./synthetic-observations/synthetic-obs-T{0}".format(T), obs)
+    tf = time.time()
+    print("Synthetic data generated. Time: {0}".format(tf-t0))
+
+for T in [10, 20, 50, 100, 500, 1000]:
+    generate_states(T)
+    generate_data(T)
 
 
 # generate initial ensemble
