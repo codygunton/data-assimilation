476d27b426c4eeb22197c3da2648ed087699b1f3
Cody Gunton <codygunton@gmail.com>
2017-11-17 13:47:59 -0700

    Attempt at using different perturb for each ens member. Bad RMSEs.

--- ensemble-kalman-filter/perturbed-obs.py
+++ ensemble-kalman-filter/perturbed-obs.py
@@ -7,8 +7,9 @@ import multiprocessing as mp
 # specify simultation length. options: T = 10, 20, 50, 100, 500, 1000
 T = 20
 # specify ensemble size. options: 20, 40, 100, 200, 500
-Ne = 100
+Ne = 20
 
+# all other parameters are derived
 
 # load states and initial ensemble
 true_states = np.load("./true-states/true-states-T{0}.npy".format(T))
@@ -45,15 +46,21 @@ def M(A, h):
     return A + (h/6)*(k1 + 2*k2 + 2*k3 + k4)
 
 
-# observations happen every obs_gap seconds, which is n RK steps;
-# we need to iterate M that many times
-def Mn(A):  # want to use map below so i assume h is defined
+# observations happen n RK steps; we need to iterate M that many times
+def Mn(A):  # assume h is defined so i can (easily) use map below
     for i in range(n):
         A = M(A, h)
     return A
 
 
 # kalman filter
+#   inputs: inflation parameter alpha, localization parameter r
+#   output: five-tuple ensembles, mu_as, P_as, RMSEs, spreads
+#   ensembles: list containing initial ensemble and all analysis ensembles
+#   mu_as: list of analysis means cas as array of shape (no. obs, N) 
+#   P_as: list of analysis covariacnes cas asarray of shape (no. obs, N, N)
+#   RMSEs
+
 def main(alpha, r, log_name):
     t0 = time.time()
     ensembles = [ens_0]
@@ -61,24 +68,24 @@ def main(alpha, r, log_name):
     P_as = []
 
     # localization function
-    D = sum([np.eye(N, k=d) * np.exp(-(d/r)**2)
+    D = sum([np.eye(N, k=d) * np.exp(-abs(d/r))
              for d in range(-N+1, N)])
-    E = sum([np.eye(N, k=d) * np.exp(-(abs(abs(N-1)-abs(d))/r)**2)
+    E = sum([np.eye(N, k=d) * np.exp(-(abs(abs(N-1)-abs(d))/r))
              for d in range(-N+1, N)])
     L = (E + D)/(E + D)[0][0]
 
-    # generate perturbed observations
-    np.random.seed(2018)
-    epsilon = np.array([np.random.multivariate_normal(np.zeros(N//2), R)
-                        for y in obs])
-    perturbed_obs = obs + epsilon
+    # # generate perturbed observations
+    # np.random.seed(2018)
+    # epsilon = np.array([np.random.multivariate_normal(np.zeros(N//2), R)
+    #                     for y in obs])
+    # perturbed_obs = obs + epsilon
 
     y = obs[0]
-    ty = perturbed_obs[0]
+    # ty = perturbed_obs[0]  #!! x add different noise for each ensemble
 
     for t in range(len(obs)-1):
         y = obs[t+1]
-        ty = perturbed_obs[t+1]
+        # ty = perturbed_obs[t+1] #!! x add different noise for each ensemble
 
         # get previous ensemble and analysis mean and covariance
         ens = ensembles[-1]
@@ -103,10 +110,20 @@ def main(alpha, r, log_name):
         K = np.linalg.solve(H @ P_f_loc.T @ H.T + R.T, H @ P_f_loc.T).T
 
         # analysis
-        ens_a = map(lambda e: e + K @ (ty - H @ e), infl_ens_f)
-        # tilde should be here; should be on infl_ens_f?
+        ens_a = []
+        mu_a = []
+        for e in infl_ens_f:
+            ty = np.random.multivariate_normal(np.zeros(N//2), R)
+            ens_a.append(e + K @ (ty - H @ e))
         ens_a = np.array(list(ens_a))
-        mu_a = mu_f + K @ (ty - H @ mu_f)  # should use tilde-y?
+        mu_a = np.mean(ens_a, axis=0)
+
+        # # formerly
+        # ens_a = map(lambda e: e + K @ (ty - H @ e), infl_ens_f)
+        # # tilde should be here; should be on infl_ens_f?
+        # ens_a = np.array(list(ens_a))
+        # mu_a = mu_f + K @ (ty - H @ mu_f)  # should use tilde-y?
+
         # P_a = (np.identity(N)-K @ H) @ P_f_loc
         P_a = (1/(Ne-1)) * sum([np.outer((e - mu_f), (e - mu_f).T)
                                 for e in ens_a])
@@ -122,37 +139,24 @@ def main(alpha, r, log_name):
 
     RMSEs = list(map(lambda A: np.sqrt(sum(A)/N),
                      (mu_as-x_ts)**2))
-
     spreads = [np.sqrt(np.trace(P)/N) for P in P_as]
 
-    # # show RMSEs
-    # plt.figure(figsize=(12, 6))
-    # times = range(len(RMSEs))
-    # plt.scatter(times, RMSEs, s=10, c='#ff6699')
-    # plt.show()
-
-    # # show spreads
-    # plt.figure(figsize=(12, 6))
-    # times = range(len(spreads))
-    # plt.scatter(times, spreads, s=10, c='#ff6699')
-    # plt.show()
-
     # discard data points from spin-up time
-    RMSEs = RMSEs[len(obs)//2:]
-    spreads = spreads[len(obs)//2:]
+    good_RMSEs = RMSEs[len(obs)//2:]
+    good_spreads = spreads[len(obs)//2:]
 
     t1 = time.time()
 
     log_file = open(log_name, 'a')
     log_file.write("[{0}, {1}, {2}, {3}]\n"
-                   .format(alpha, r, np.mean(RMSEs), t1-t0))
+                   .format(alpha, r, np.mean(good_RMSEs), t1-t0))
     log_file.close()
 
     print("\n α = {0}, r = {1} "
           " =>   mean RMSE after spin-up = {2} \n runtime: {3}"
-          .format(alpha, r, np.mean(RMSEs), t1-t0))
+          .format(alpha, r, np.mean(good_RMSEs), t1-t0))
 
-    return ensembles, mu_as, P_as, RMSEs, spreads
+    return ensembles, mu_as, P_as, good_RMSEs, spreads
 
 
 def iterate_main(param_set, log_name, q):  # q is instance of mp.Queue()
@@ -163,45 +167,91 @@ def iterate_main(param_set, log_name, q):  # q is instance of mp.Queue()
     q.put(RMSE_info)
 
 
-ensembles, mu_as, P_as, RMSEs, spreads = main(0.16, 5.5, 'whatever')
+# for testing
+alpha = 0.16
+r = 5.5
+ensembles, mu_as, P_as, RMSEs, spreads = main(alpha, r, 'whatever')
+
+# plot analysis mean of the i-th variable
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+i = 1
+fig = plt.figure()
+plt.suptitle('True $x_0$ data and Kalman reconstruction',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (seconds)',
+                  fontsize=14, fontweight='bold')
+plt.ylabel('position', fontsize=14, fontweight='bold')
+times = np.arange(0, T, h)
+plt.plot(times[n::n], mu_as[:, i-1])
+plt.plot(times[n::n], true_states[n::n, i-1])
+plt.legend(['True $x_0$', 'Kalman reconstruction'], markerscale=4)
+# plt.set_xlabel('$x_{0}$'.format(i))
+plt.show()
+
+# plot RMSEs
+plt.figure(figsize=(12, 6))
+plt.suptitle('RMSEs',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (number of observation intervals)',
+                  fontsize=14, fontweight='bold')
+times = range(len(RMSEs))
+plt.scatter(times, RMSEs, s=10)
+plt.show()
+
+# plot spreads
+plt.figure(figsize=(12, 6))
+plt.suptitle('Spreads',
+             fontsize=14, fontweight='bold')
+plt.xlabel(r"$t$"' (number of observation intervals)',
+                  fontsize=14, fontweight='bold')
+times = range(len(spreads))
+plt.scatter(times, spreads, s=10)
+plt.show()
 
 
-if __name__ == "__main__":
-    alpha_lower = 0
-    alpha_upper = 0.5
-    alpha_step = 0.02
-    r_lower = 1
-    r_upper = 6
-    r_step = 0.5
-    params = [(alpha, r)
-              for alpha in
-              np.arange(alpha_lower, alpha_upper, alpha_step)
-              for r in
-              np.arange(r_lower, r_upper, r_step)]
+# if __name__ == "__main__":
+#     alpha_lower = 0
+#     alpha_upper = 0.5
+#     alpha_step = 0.02
+#     r_lower = 1
+#     r_upper = 6
+#     r_step = 0.5
+#     params = [(alpha, r)
+#               for alpha in
+#               np.arange(alpha_lower, alpha_upper, alpha_step)
+#               for r in
+#               np.arange(r_lower, r_upper, r_step)]
 
-    # set up log
-    log_name = 'po-T{0}-Ne{1}'.format(T, Ne)
-    log_file = open(log_name, 'w')
-    log_file.write("T = {4}, Ne = {0}, N = {1}, h = {2}, n = {3}\n"
-                   .format(Ne, N, h, n, T))
-    log_file.write("alpha range: ({0})\n"
-                   .format((alpha_lower, alpha_lower, alpha_step)))
-    log_file.write("r range: ({0})\n"
-                   .format((r_lower, r_upper, r_step)))
-    log_file.close()
+#     # set up log
+#     log_name = 'po-T{0}-Ne{1}'.format(T, Ne)
+#     log_file = open(log_name, 'w')
+#     log_file.write("T = {4}, Ne = {0}, N = {1}, h = {2}, n = {3}\n"
+#                    .format(Ne, N, h, n, T))
+#     log_file.write("alpha range: ({0})\n"
+#                    .format((alpha_lower, alpha_lower, alpha_step)))
+#     log_file.write("r range: ({0})\n"
+#                    .format((r_lower, r_upper, r_step)))
+#     log_file.close()
 
-    # get RMSEs
-    all_RMSEs = []
-    for i in range(len(params)//5+1):
-        q = mp.Queue()
-        param_set = params[5*i:5*(i+1)]
-        p = mp.Process(name='iterate_main',
-                       target=iterate_main,
-                       args=(param_set, log_name, q))
-        p.start()
-        RMSE_info = q.get()
-        all_RMSEs += RMSE_info
-        p.join()
+#     # get RMSEs
+#     all_RMSEs = []
+#     for i in range(len(params)//5+1):
+#         q = mp.Queue()
+#         sparam_set = params[5*i:5*(i+1)]
+#         p = mp.Process(name='iterate_main',
+#                        target=iterate_main,
+#                        args=(param_set, log_name, q))
+#         p.start()
+#         RMSE_info = q.get()
+#         all_RMSEs += RMSE_info
+#         p.join()
 
-    log_file.close()
-    np.save('po-T{0}-Ne{1}-RMSEs'.format(T, Ne), np.array(all_RMSEs))
+#     log_file.close()
+#     np.save('po-T{0}-Ne{1}-RMSEs'.format(T, Ne), np.array(all_RMSEs))
+
+
+
+
+# # load old good_RMSEs file for analysis
+# good_RMSEs = np.load('hpc-files/po-T20-Ne20-RMSEs.npy')
